a40cb81f69a7ca6a5e87cf1d5ad5171edced3bd5
Ondrej Telka <ondrejtelka@gmail.com>
2021-02-20 18:55:20 +0100

    Implement our own youtube uploader

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM ubuntu:latest
-RUN apt-get -y update
+RUN apt-get -y update --fix-missing
 
 # fix some TZ issues
 ENV TZ 'Europe/Prague'
@@ -11,35 +11,36 @@ dpkg-reconfigure -f noninteractive tzdata && \
 apt-get clean
 
 # install dependencies
-RUN apt-get -y install git xvfb firefox-geckodriver firefox python3 python3-pip python3-venv
-RUN apt-get -y install git nodejs npm ffmpeg
+RUN apt-get -y install git wget sudo
+RUN apt-get -y install xvfb firefox-geckodriver firefox python3 python3-pip python3-venv
+RUN apt-get -y install nodejs npm ffmpeg
 RUN apt-get -y install libnss3 libgbm1
 
 # add a non-root user
 RUN useradd -m user
-USER user
-WORKDIR /home/user
+#USER user
+WORKDIR /src
 
 # install youtube uploader
-RUN git clone https://github.com/linouk23/youtube_uploader_selenium.git
-RUN pip3 install -r /home/user/youtube_uploader_selenium/requirements.txt
+RUN git clone https://github.com/FEL-CVUT-Reupload/youtube_uploader_selenium.git
+RUN pip3 install -r /src/youtube_uploader_selenium/requirements.txt
 
 # install destreamer
 RUN git clone https://github.com/FEL-CVUT-Reupload/destreamer.git
-WORKDIR /home/user/destreamer
+WORKDIR /src/destreamer
 RUN npm install
 RUN npm run build
-WORKDIR /home/user
+WORKDIR /src
+RUN chown -R user:user /src/destreamer
 
 # install sharepoint downloader
 RUN git clone https://github.com/FEL-CVUT-Reupload/sharepoint_downloader.git
-WORKDIR /home/user/sharepoint_downloader
+WORKDIR /src/sharepoint_downloader
 RUN npm install
-WORKDIR /home/user
+WORKDIR /src
 
 # reuploader script
 COPY reuploader.sh reuploader.sh
-
 
 # CMD ["/usr/bin/bash"]
 CMD ["/usr/bin/bash", "reuploader.sh"]
--- reuploader.sh
+++ reuploader.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
+export LANG=C.UTF-8
 
 read -p "Subject code: " -e subject
 subject=$(echo "$subject" | tr '[:lower:]' '[:upper:]')
@@ -38,21 +39,33 @@ echo "  6. sharepoint (teams)"
 read -p "Source: " -e source
 read -p "URL/URI: " -e url
 
-case $source in
-	3|6) # microsoft stream (teams)
-		read -p "ČVUT username: " -e username
-		read -p "ČVUT password: " -e password
-	;;
-	5) # youtube livestream
-		read -p "Recording time: " -i "2h" -e rectime
-	;;
-esac
 
+if [[ "$source" == "6" ]] || [[ ! -f cookies/youtube.com.pkl ]]; then
+	login=true
+fi
+
+if [[ "$source" == "3" ]] && test "$(find cookies/.token_cache -mmin +60 2>/dev/null)"; then
+	login=true
+fi
+
+if [[ -n "$login" ]]; then
+	read -p "ČVUT username: " -e username
+	read -s -p "ČVUT password: " -e password
+fi
+
+if [[ "$source" == "5" ]]; then
+	read -p "Recording time: " -i "2h" -e rectime
+fi
+
+
+test -n "$lectitle" && lectitle=": $lectitle"
 
 echo
-
-
-
+echo -e "\033[34m"
+echo "Title: $subject $lecnum. $category1 ($datestr)$lectitle"
+echo "Description: $description"
+echo "Playlist: $subject $categoryN [$semester]"
+echo -e "\033[0m"
 
 
 # not bigger than 1080p, vp9 preferred
@@ -65,6 +78,7 @@ case $source in
 	1) # any video url
 		tmp_filename="video.${url##*.}"
 		wget "$url" -O "$tmp_filename"
+		tmp_filename=$(readlink -f "$tmp_filename")
 	;;
 	2) # bbb-internal
 		echo "bbb-internal is not implemented yet."
@@ -72,16 +86,26 @@ case $source in
 		cd bbb-recorder/
 		tmp_filename="video.webm"
 		node export.js "$url" "$tmp_filename"
+		tmp_filename=$(readlink -f "$tmp_filename")
+		cd ../
 	;;
 	3) # microsoft stream (teams)
+		if [[ -f cookies/.token_cache ]]; then
+			cp -v cookies/.token_cache destreamer/
+			chown user:user destreamer/.token_cache
+		fi
 		cd destreamer/
-		xvfb-run ./destreamer.sh --outputDirectory "." --format mp4 -x -k -u "$username@cvut.cz" -p "$password" -i "$url"
+		sudo -u user xvfb-run ./destreamer.sh --outputDirectory "." --format mp4 -x -k -u "$username@cvut.cz" -p "$password" -i "$url"
 		tmp_filename=$(ls -t | grep -E "*.mp4$" | head -n1)
+		tmp_filename=$(readlink -f "$tmp_filename")
+		cd ../
+		cp -uv destreamer/.token_cache cookies/.token_cache
 	;;
 	4) # youtube static
 		# tmp_filename=$(youtube-dl -f "$ytformat" --get-filename "$url" -o "video.%(ext)s")
 		youtube-dl --fragment-retries infinite -f "$ytformat" "$url" -o "video.%(ext)s"
 		tmp_filename=$(ls -t | grep -E "video.*$" | head -n1)
+		tmp_filename=$(readlink -f "$tmp_filename")
 	;;
 	5) # youtube livestream
 		tmp_filename=$(youtube-dl -f "$ytformat" --get-filename "$url" -o "video.%(ext)s")
@@ -95,10 +119,13 @@ case $source in
 		
 		# wait for the youtube-dl to spin down
 		sleep 20s
+
+		tmp_filename=$(readlink -f "$tmp_filename")
 	;;
 	6) # sharepoint
-		tmp_filename="~/sharepoint_downloader/video"
+		tmp_filename="sharepoint_downloader/video"
 		node ./sharepoint_downloader/index.js -u "$username" -p "$password" -i "$url" -o "$tmp_filename"
+		tmp_filename=$(readlink -f "$tmp_filename")
 	;;
 	*)
 		echo -e "\033[31mUnknown video source!\033[0m"
@@ -107,19 +134,21 @@ case $source in
 esac
 
 
+# upload
+[[ -z "$login" ]] && cp -v cookies/youtube.com.pkl youtube_uploader_selenium/
+cd youtube_uploader_selenium/
+python3 upload.py \
+	--video="$tmp_filename" \
+	--channel="UCzQzKzGlpX4qBi060qdwIug" \
+	--title="$subject $lecnum. $category1 ($datestr)$lectitle" \
+	--description="$description" \
+	--playlist="$subject $categoryN [$semester]" \
+	--privacy="unlisted" \
+	--username="$username" \
+	--password="$password" \
+	--headless
 
-
-test -n "$lectitle" && lectitle=": $lectitle"
-
-echo "Title: $subject $lecnum. $category1 ($datestr)$lectitle"
-echo "Description: $description"
-echo "Playlist: $subject $categoryN $YEAR"
-
-
-
-
-exit 99
-youtube-upload --title="$subject $lecnum. $category1 ($datestr)$lectitle" --description="$description" --playlist="$subject $categoryN [$semester]" --category="Education" --privacy=unlisted --default-language="cs" --default-audio-language="cs" "$filename"
+[[ -f youtube.com.pkl ]] && cp -uv youtube.com.pkl ../cookies/
 
 echo -e "\n\033[32mDONE\033[0m"
 
