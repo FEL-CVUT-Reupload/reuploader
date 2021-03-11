3311a39058bd270688ced77b8d914d1badc2f3c3
Ondrej Telka <ondrejtelka@gmail.com>
2021-03-12 00:29:29 +0100

    Create separate browser cache folders

--- reuploader.sh
+++ reuploader.sh
@@ -4,8 +4,11 @@ NODE_EXEC=${NODE_EXEC:-"node"}
 set -e
 export LANG=C.UTF-8
 
-cache_path="/mnt"
 channel="UCzQzKzGlpX4qBi060qdwIug"
+cache_path="/mnt"
+mkdir -p "$cache_path/destreamer"
+mkdir -p "$cache_path/sharepoint"
+mkdir -p "$cache_path/uploader"
 
 while true; do
 	echo "Metadata template:"
@@ -99,12 +102,12 @@ echo
 echo "Logging in to YouTube..."
 while true; do
 	set +e
-	python3 youtube_uploader_selenium/login.py --channel="$channel" --username="$cvut_username" --password="$cvut_password" --cookies="$cache_path" --headless
+	python3 youtube_uploader_selenium/login.py --channel="$channel" --username="$cvut_username" --password="$cvut_password" --cookies="$cache_path/uploader" --headless
 	status="$?"
 	set -e
 	if [[ "$status" == "7" ]]; then
-		read -r -p "ČVUT username: "  -e cvut_username
-		read -r -s -p "ČVUT password: "  -e cvut_password
+		read -r -p "ČVUT username: "   -e cvut_username
+		read -r -s -p "ČVUT password: "   -e cvut_password
 		echo
 	else
 		break
@@ -150,14 +153,16 @@ case "$source" in
 	"microsoft stream (teams)")
 		cd destreamer/
 
+		chown user:user -R "$cache_path/destreamer"
+
 		while true; do
 			set +e
-			sudo -u user NODE_EXEC="$NODE_EXEC" xvfb-run ./destreamer.sh --outputDirectory "." --format mp4 -x -u "$cvut_username@cvut.cz" -p "$cvut_password" -k -c "$cache_path" -i "$url"
+			sudo -u user NODE_EXEC="$NODE_EXEC" xvfb-run ./destreamer.sh --outputDirectory "." --format mp4 -x -u "$cvut_username@cvut.cz" -p "$cvut_password" -k -c "$cache_path/destreamer" -i "$url"
 			status="$?"
 			set -e
 			if [[ "$status" == "7" ]]; then
-				read -r -p "ČVUT username: "  -e cvut_username
-				read -r -s -p "ČVUT password: "  -e cvut_password
+				read -r -p "ČVUT username: "   -e cvut_username
+				read -r -s -p "ČVUT password: "   -e cvut_password
 				echo
 			else
 				break
@@ -195,12 +200,12 @@ case "$source" in
 
 		while true; do
 			set +e
-			$NODE_EXEC ./sharepoint_downloader/index.js -u "$cvut_username" -p "$cvut_password" --chromeData "$cache_path" -i "$url" -o "$tmp_filename"
+			$NODE_EXEC ./sharepoint_downloader/index.js -u "$cvut_username" -p "$cvut_password" --chromeData "$cache_path/sharepoint" -i "$url" -o "$tmp_filename"
 			status="$?"
 			set -e
 			if [[ "$status" == "7" ]]; then
-				read -r -p "ČVUT username: "  -e cvut_username
-				read -r -s -p "ČVUT password: "  -e cvut_password
+				read -r -p "ČVUT username: "   -e cvut_username
+				read -r -s -p "ČVUT password: "   -e cvut_password
 				echo
 			else
 				break
@@ -228,7 +233,7 @@ python3 youtube_uploader_selenium/upload.py \
 	--description="$video_description" \
 	--playlist="$video_playlist" \
 	--privacy="$video_visibility" \
-	--cookies="$cache_path" \
+	--cookies="$cache_path/uploader" \
 	--headless
 
 echo -e "\n\033[32mDONE\033[0m"
